b90cd47e2c77194eae2569bad463b7da8d5ab5a4
unknown <l.shvelidze09@gmail.com>
2024-09-24 18:29:06 +0400

    push first vs core project

---
+++ Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Lizi Shvelidze");
+        }
+    }
+}
